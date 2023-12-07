67ab5e3b7dfa04e2081d5764b7f4e4256281c113
MegsunIvanova <megsunmeg@gmail.com>
2023-12-07 10:35:08 +0200

    src from my first project  - some new Interceptor - LoggerInterceptor

--- src/main/java/bg/softuni/autho_moto_manager/configuration/WebConfiguration.java
+++ src/main/java/bg/softuni/autho_moto_manager/configuration/WebConfiguration.java
@@ -1,20 +1,20 @@
 package bg.softuni.autho_moto_manager.configuration;
 
-import bg.softuni.autho_moto_manager.service.impl.ModifyingPermissionInterceptor;
+import bg.softuni.autho_moto_manager.service.impl.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-    private final ModifyingPermissionInterceptor modifyingPermissionInterceptor;
+    private final LoggerInterceptor loggerInterceptor;
 
-    public WebConfiguration(ModifyingPermissionInterceptor modifyingPermissionInterceptor) {
-        this.modifyingPermissionInterceptor = modifyingPermissionInterceptor;
+    public WebConfiguration(LoggerInterceptor loggerInterceptor) {
+        this.loggerInterceptor = loggerInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(modifyingPermissionInterceptor);
+        registry.addInterceptor(loggerInterceptor);
     }
 }
